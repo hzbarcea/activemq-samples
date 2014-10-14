e189e4d3d365b58b38767ce35b53285fe37fa15c
Hadrian Zbarcea <hadrian@apache.org>
2014-10-14 14:53:46 -0400

    User broker instead of node in the api

--- services/orbweaver-cc/src/main/java/org/example/activemq/orbweaver/cc/ControlCenterService.java
+++ services/orbweaver-cc/src/main/java/org/example/activemq/orbweaver/cc/ControlCenterService.java
@@ -2,10 +2,34 @@
  */
 package org.example.activemq.orbweaver.cc;
 
+import org.example.activemq.orbweaver.api.BrokerType;
+import org.example.activemq.orbweaver.api.BrokersType;
+import org.example.activemq.orbweaver.api.ControlCenter;
+
 
 /**
- * JAX-RS ControlCenterService root resource
+ * JAX-RS ControlCenter root resource
  */
-public class ControlCenterService {
+public class ControlCenterService implements ControlCenter {
+
+	public BrokersType showBrokers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public BrokerType showBroker(String brokerid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void updateBroker(String brokerid, BrokerType brokertype) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteBroker(String brokerid) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
